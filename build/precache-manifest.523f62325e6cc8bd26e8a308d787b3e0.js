e58c7bf0f3eb417ce91aab239b1006358fe502fe
Nimish Kapoor <nimish.kapoor@publicissapient.com>
2020-01-04 18:06:48 +0000

    added files for github pages

---
+++ build/precache-manifest.523f62325e6cc8bd26e8a308d787b3e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e78d87235db9fe52b6d6915257bd71",
+    "url": "/React-Sample-App/index.html"
+  },
+  {
+    "revision": "e115cd1528f042ebef56",
+    "url": "/React-Sample-App/static/css/main.d86419df.chunk.css"
+  },
+  {
+    "revision": "25f4a5800515bf04f7ae",
+    "url": "/React-Sample-App/static/js/2.6e883175.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Sample-App/static/js/2.6e883175.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e115cd1528f042ebef56",
+    "url": "/React-Sample-App/static/js/main.88bbd6c3.chunk.js"
+  },
+  {
+    "revision": "cb9270ae93b42b9ab42b",
+    "url": "/React-Sample-App/static/js/runtime-main.b8cec495.js"
+  }
+]);
